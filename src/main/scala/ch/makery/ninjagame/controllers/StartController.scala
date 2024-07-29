40a74d0df43180818a6bd4e696d4c26e60e12dbf
bowie <bowiechew03@gmail.com>
2024-07-29 21:18:58 +0800

    added music

--- src/main/scala/ch/makery/ninjagame/controllers/StartController.scala
+++ src/main/scala/ch/makery/ninjagame/controllers/StartController.scala
@@ -1,18 +1,24 @@
 package scala.ch.makery.ninjagame.controllers
 
-import scalafx.scene.control.Button
+import scalafx.scene.control.{Button, TextField}
 import scalafxml.core.macros.sfxml
 import javafx.event.ActionEvent
+
 import scala.ch.makery.ninjagame.MainApp
 
-
-
 @sfxml
-class StartController(private val startButton: Button) {
+class StartController(private val startButton: Button,
+                      private val leaderboardButton: Button,
+                      private val nicknameField: TextField
+                     ) {
 
   def onStartButtonClick(event: ActionEvent): Unit = {
-    println("Start button clicked")
-    MainApp.switchToGameScene()
+    val nickname = nicknameField.text.value.trim
+    if (nickname.nonEmpty) {
+      MainApp.startGame(nickname)
+    } else {
+      println("Nickname cannot be empty")
+    }
   }
 
 
